9e176bea8e72bff51968aab06d9e984813d943d1
Krzysztof Komorowski <k.komorowski@oberthur.com>
2017-08-07 16:30:20 +0200

    Working with Chromedriver

--- src/test/scala/pl/hiquality/seleniumscala/HiQualitySpec.scala
+++ src/test/scala/pl/hiquality/seleniumscala/HiQualitySpec.scala
@@ -1,17 +1,21 @@
 package pl.hiquality.seleniumscala
 
+import org.junit.runner.RunWith
 import org.openqa.selenium.WebDriver
-import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.scalatest.junit.JUnitRunner
 import org.scalatest.selenium.WebBrowser
-import org.scalatest.{WordSpec, Matchers}
+import org.scalatest.{Matchers, WordSpec}
 
-object HiQualitySpec extends WordSpec with Matchers with WebBrowser {
-  implicit val webDriver: WebDriver = new FirefoxDriver()
+@RunWith(classOf[JUnitRunner])
+class HiQualitySpec extends WordSpec with Matchers with WebBrowser {
+  implicit val webDriver: WebDriver = new ChromeDriver()
 
   "The Blog home page" should {
     "have correct title" in {
       go to "http://hiquality.pl"
-      pageTitle should be ("Hi, Quality!")
+      pageTitle should be("Hi, Quality!")
+      webDriver.close()
     }
   }
 }
